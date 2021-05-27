35f00d1e1208ebf33df836b60fe23f3670d6d462
EKhoroshko <xapoxa14@gmail.com>
2021-05-27 16:15:25 +0300

    All worked

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ const modalImg = document.querySelector(".lightbox__image");
 const closeModalBtn = document.querySelector("button[data-action='close-lightbox']");
 const overlay = document.querySelector('.lightbox__overlay');
 
+const imagesSrc = [];
+imagesArray.forEach(el => {
+    imagesSrc.push(el.original);
+});
+
 function createGallery() {
     const itemRef = imagesArray.map(image => {
 
@@ -28,22 +33,41 @@ function createGallery() {
 }
 createGallery(imagesArray);
 
-
-galleryRef.addEventListener('click', onClickInImage);
+galleryRef.addEventListener('click', onClickImgOpen);
 closeModalBtn.addEventListener('click', onClickCloseModal);
 overlay.addEventListener('click', onClickOverlayClose);
+window.addEventListener('keydown', onClickEscClose);
 
-function onClickInImage(e) {
-    e.preventDefault();
-    if (e.target.nodeName !== 'IMG') {
+function onClickImgOpen(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
         return;
     }
-    window.addEventListener('keydown', onClickEscClose);
 
-    const imageRef = e.target;
-    modalImg.src = imageRef.dataset.source;
-    modalImg.alt = imageRef.alt;
+    const imgEl = event.target;
+    modalImg.src = imgEl.dataset.source;
+    modalImg.alt = imgEl.alt;
     modalEl.classList.add('is-open');
+
+
+    document.addEventListener('keydown', (e) => {
+
+        let newIndex = imagesSrc.indexOf(modalImg.src);
+        if (e.key === 'ArrowLeft') {
+            newIndex -= 1;
+            if (newIndex === -1) {
+                newIndex = imagesSrc.length - 1;
+            }
+        } else if (e.key === 'ArrowRight') {
+            newIndex += 1;
+            if (newIndex === imagesSrc.length) {
+                newIndex = 0;
+            }
+        }
+
+        modalImg.src = imagesSrc[newIndex];
+    });
+
 }
 
 function onClickCloseModal() {
@@ -62,9 +86,10 @@ function onClickOverlayClose(e) {
 }
 
 function onClickEscClose(e) {
-    if (e.code === 'Escape') {
-        onClickCloseModal();
+    if (e.key !== 'Escape') {
+        return;
     }
+    modalEl.classList.remove('is-open');
     modalImg.src = "";
     modalImg.alt = "";
 }
@@ -90,28 +115,3 @@ function createList(imagesArray) {
 }*/
 
 
-const imagesSrc = [];
-imagesArray.forEach(el => {
-    imagesSrc.push(el.original);
-});
-
-/*console.dir(imagesSrc[6]);*/
-
-document.addEventListener('keydown', (e) => {
-
-    let newIndex = imagesSrc.indexOf(modalImg.src);
-    if (e.key === 'ArrowLeft') {
-        newIndex -= 1;
-        if (newIndex === -1) {
-            newIndex = imagesSrc.length - 1;
-        }
-    }/*else if (e.key === 'ArrowRight') {
-        newIndex += 1;
-        if (newIndex === imagesSrc.length) {
-            newIndex = 0;
-        }
-    }*/
-    console.dir(imagesSrc[6]);
-
-    modalImg.src = imagesSrc[newIndex];
-});
